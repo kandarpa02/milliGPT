e6d324fffebaee11c0d64c5f43d2a1b12ff2df9e
kandarpa02 <kandarpaexe@gmail.com>
2025-05-29 23:54:43 +0530

    Args position mismatch fixed

--- tests/attn_test.py
+++ tests/attn_test.py
@@ -6,6 +6,6 @@ def test():
     x, k1 = random.split(key)
     X = random.normal(x, (1, 6, 4))
     params = [random.normal(k1, (4, 4)) for _ in range(4)]
-    y = multi_head_attention(X, params, 4)
+    y = multi_head_attention(params, X, 4)
     assert y.shape == (1, 6, 4)
 
