06e26ba987f09538259d4ec064c5104871abd7d4
kandarpa02 <kandarpaexe@gmail.com>
2025-05-31 11:18:52 +0530

    defined micro_gpt_1, 1 transformer block

--- main/stack/gpt_micro.py
+++ main/stack/gpt_micro.py
@@ -45,7 +45,7 @@ class micro_gpt_1:
             "embed"   : init_embedding_params(42, self.vocab, self.model_d),
             "attn1"   : init_attention_param(self.model_d, "attn1"),
             "ln1"     : init_layer_norm_params(self.model_d, "ln1"),
-            "ffn1_fc" : init_linear_param(self.model_d, 2 * self.model_d, "ffn1_fc"),
+            "ffn1_fc" : init_linear_param(self.model_d, 4 * self.model_d, "ffn1_fc"),
             "ffn2_fc" : init_linear_param(4 * self.model_d, self.model_d, "ffn2_fc"),
 
         }
